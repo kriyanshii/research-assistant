389b304d3e51a6927e431f509810d9e4e879fa98
Nimit <nimit.shah622@gmail.com>
2023-04-02 11:14:44 +0530

    major changes to main.py (converted to Main class) changes to app.py now allow users to query existing knowledge finally error.html added to show an error screen on compute errors

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+# type: ignore
+
 from flask import Flask, render_template
 import flask
 import os
-from main import init
+from main import Main
 
 app = Flask(__name__)
+compute = Main()
 
 UPLOAD_FOLDER = 'data'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -14,16 +17,24 @@ def upload():
         files = flask.request.files.getlist("file")
         for file in files:
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        try:
+            compute.uploadDocuments(app.config['UPLOAD_FOLDER'])
+        except:
+        return render_template("error.html")
 
     return render_template("prompt.html")
 
-@app.route("/prompt", methods = ['POST'])
+@app.route("/prompt", methods = ['GET', 'POST'])
 def prompt():
+    if flask.request.method == "GET":
+        return render_template("prompt.html")
+    
     if flask.request.method == "POST":
         queryText = flask.request.form.get('promptText')
-
-        output = init(queryText)
-
+        try:
+            output = compute.query(queryText)
+        except:
+            return render_template("error.html")
         return render_template("prompt.html", output = output)
 
 
@@ -32,4 +43,5 @@ def prompt():
 def home():
     return render_template('index.html')
 
-app.run(debug=False)
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,114 +1,134 @@
+# type: ignore
+
 from sentence_transformers import SentenceTransformer
 from tqdm.auto import tqdm
 from PyPDF2 import PdfReader
 import pinecone, openai
 import re, os, shutil
+from time import sleep
 
+class Main:
 
-def init(queryText):
-    model = SentenceTransformer('sentence-transformers/multi-qa-mpnet-base-dot-v1')
+    def __init__(self):
+        print('main init')
+        self.initialized = False
+        self.uploaded = True
+        self.model = SentenceTransformer('sentence-transformers/multi-qa-mpnet-base-dot-v1')
 
-    ### CAN USE ENVIRONMENT VARIABLES FOR THIS
-    openai.api_key = os.environ.get("OPENAI_KEY")
-    pinecone.init(
-        api_key= os.environ.get("PINECONE_API_KEY"),
-        environment='eu-west1-gcp',
-    )
-    index_name = 'user-index-nimit'
-    ###
+        openai.api_key = os.environ.get("OPENAI_KEY")
+        pinecone.init(
+            api_key=os.environ.get("PINECONE_API_KEY"),
+            environment='eu-west1-gcp',
+        )
+        index_name = 'user-index-nimit'
+        ###
 
-    if index_name not in pinecone.list_indexes():
-      pinecone.create_index(
-          index_name,
-          dimension=768,
-          metric='dotproduct',
-          metadata_config={'indexed': []}
-      )
-
-    ## GET INDEX
-    index = pinecone.Index(index_name)
-    print(index.describe_index_stats())
-    
-    for filename in os.listdir('data'):
-      total_vectors = index.describe_index_stats()['total_vector_count']
-      f = filename.split('.')
-      title = ''.join(f[:-1])
-      if(f[-1] != 'pdf'):
-         raise NotImplementedError
-
-      path = f'data/{filename}'
-      sentences = getCorpus(path)
-      try:
-        shutil.move(path, 'used_data')
-      except:
-        os.remove(path)
-    #   shutil.move(path, 'used_data')
-
-    #   try:
-    #     shutil.move(path, 'used_data')
-    #   except: 
-    #     pass
-      
-      
-      print(f'Vectors for {title}:', len(sentences))
-
-      ids = list(map(str, [*range(total_vectors, total_vectors + len(sentences))]))
-      embeds = [model.encode(sentence, convert_to_numpy=True).tolist() for sentence in sentences] # type: ignore
-      meta = [{'title': title, 'text': sentence} for sentence in sentences]
-
-      to_upsert = list(zip(ids, embeds, meta))
-
-      # print(to_upsert[0])
-
-      index.upsert(vectors=to_upsert)
-    query = queryText
-    # query = 'question answering capability of gpt-2'
-    qv = model.encode(query).tolist() # type: ignore
-
-    completionResult = index.query(qv, top_k=3, include_metadata=True)
-    # print(res)
-    
-    contexts = [x['metadata']['text'] for x in completionResult['matches']]
-    prompt_start = (
-        "Answer the question based on the context below.\n\n"+
-        "Context:\n"
-    )
-    prompt_end = (
-        f"\n\nQuestion: {query}\nAnswer:"
-    )
-    prompt = ''
-    limit = 3750
-    for i in range(1, len(contexts)):
-        if len("\n\n---\n\n".join(contexts[:i])) >= limit:
-            prompt = (
-                prompt_start +
-                "\n\n---\n\n".join(contexts[:i-1]) +
-                prompt_end
-            )
-            break
-        elif i == len(contexts)-1:
-            prompt = (
-                prompt_start +
-                "\n\n---\n\n".join(contexts) +
-                prompt_end
+        if index_name not in pinecone.list_indexes():
+            pinecone.create_index(
+                index_name,
+                dimension=768,
+                metric='dotproduct',
+                metadata_config={'indexed': []}
             )
 
-    # print(prompt)
+        ## GET INDEX
+        self.index = pinecone.Index(index_name)
+        self.initialized = True
+        print(self.index.describe_index_stats())
+        
+    def uploadDocuments(self, upload_folder):
+        while not self.initialized:
+            print(f'initialized: {self.initialized}')
+            sleep(3)
 
-    completionResult = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt,
-        temperature=0,
-        max_tokens=400,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=None
-    )
-    ans = completionResult['choices'][0]['text'].strip() # type: ignore
-    # print('*********************************************************')
-    # print(ans)
-    return ans
+        self.uploaded = False
+        for filename in os.listdir(upload_folder):
+            total_vectors = self.index.describe_index_stats()['total_vector_count']
+            f = filename.split('.')
+            title = ''.join(f[:-1])
+
+            if(f[-1] != 'pdf'):
+                raise NotImplementedError
+
+            path = f'{upload_folder}/{filename}'
+            sentences = getCorpus(path)
+            try:
+                shutil.move(path, 'used_data')
+            except:
+                os.remove(path)
+
+            #   shutil.move(path, 'used_data')
+
+            #   try:
+            #     shutil.move(path, 'used_data')
+            #   except: 
+            #     pass
+        
+        
+            print(f'Vectors for {title}:', len(sentences))
+
+            ids = list(map(str, [*range(total_vectors, total_vectors + len(sentences))]))
+            embeds = [self.model.encode(sentence, convert_to_numpy=True).tolist() for sentence in sentences]
+            meta = [{'title': title, 'text': sentence} for sentence in sentences]
+
+            to_upsert = list(zip(ids, embeds, meta))
+            # print(to_upsert[0])
+            self.index.upsert(vectors=to_upsert)
+        
+        self.uploaded = True
+
+    def query(self, queryText):
+        while not self.initialized and not self.uploaded:
+            print(f'initialized: {self.initialized} | uploaded: {self.uploaded}')
+            sleep(3)
+
+        query = queryText
+        qv = self.model.encode(query).tolist()
+
+        completionResult = self.index.query(qv, top_k=3, include_metadata=True)
+        # print(res)
+        
+        contexts = [x['metadata']['text'] for x in completionResult['matches']]
+        prompt_start = (
+            "Answer the question based on the context below.\n\n"+
+            "Context:\n"
+        )
+        prompt_end = (
+            f"\n\nQuestion: {query}\nAnswer:"
+        )
+        prompt = ''
+        limit = 3750
+        for i in range(1, len(contexts)):
+            if len("\n\n---\n\n".join(contexts[:i])) >= limit:
+                prompt = (
+                    prompt_start +
+                    "\n\n---\n\n".join(contexts[:i-1]) +
+                    prompt_end
+                )
+                break
+            elif i == len(contexts)-1:
+                prompt = (
+                    prompt_start +
+                    "\n\n---\n\n".join(contexts) +
+                    prompt_end
+                )
+
+        # print(prompt)
+
+        completionResult = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=prompt,
+            temperature=0,
+            max_tokens=400,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
+        ans = completionResult['choices'][0]['text'].strip()
+        # print('*********************************************************')
+        # print(ans)
+        return ans
 
 
 
@@ -137,6 +157,3 @@ def getCorpus(path):
         sentences.remove(s)
     return sentences
 
-
-if __name__ == '__main__':
-    init()
